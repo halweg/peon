9faa7025cffd26cd7f9d1b6e08efcaa88294ebdb
halweg <haowei@joshine.com>
2024-02-23 18:34:51 +0800

    add router

--- framework/Routing/Router.php
+++ framework/Routing/Router.php
@@ -1,93 +1,128 @@
 <?php
 
 namespace Framework\Routing;
-use JetBrains\PhpStorm\NoReturn;
+
+use Exception;
+use Throwable;
 
 class Router
 {
-    protected array $errorHandlers = [];
-
     /**
      * @var Route[]
      */
     protected array $routes = [];
 
-    public function add(
-        string   $method,
-        string   $path,
-        callable $handler
-    ): Route
-    {
-        return $this->routes[] = new Route(
-            $method, $path, $handler
-        );
-    }
-
-    public function dispatch(): mixed
-    {
-        $requestMethod = $_SERVER['REQUEST_METHOD'] ?? "GET";
-        $requestPath   = $_SERVER['REQUEST_URI'] ?? "/";
-        $matching = $this->matches($requestMethod, $requestPath);
-        if ($matching) {
-            try {
-                return $matching->dispatch();
-            }
-            catch (\Throwable $e) {
-                return $this->dispatchError();
-            }
-        }
-        if (in_array($requestPath, $this->paths())) {
-            return $this->dispatchNotAllowed();
-        }
-        return $this->dispatchNotFound();
-    }
-
-    public function matches(string $method, string $path) : ?Route
-    {
-        foreach ($this->routes as $route) {
-            if ($route->matches($method, $path)) {
-                return $route;
-            }
-        }
-        return null;
-    }
-
     /**
-     * @return string[]
+     * @var  array<int, callable>
      */
-    public function paths() : array
-    {
-        $path = [];
-        foreach ($this->routes as $route) {
-            $path[] = $route->path();
-        }
-        return $path;
-    }
+    protected array $errorHandlers = [];
+    protected Route $current;
 
-    public function redirect(string $path): void
+    public function add(string $method, string $path, callable $handler): Route
     {
-        header("Location: {$path}", $replace = true, $code = 301);
+        return $this->routes[] = new Route($method, $path, $handler);
     }
 
     public function errorHandler(int $code, callable $handler): void
     {
         $this->errorHandlers[$code] = $handler;
     }
+
+    public function dispatch()
+    {
+        $paths = $this->paths();
+
+        $requestMethod = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+        $requestPath = $_SERVER['REQUEST_URI'] ?? '/';
+
+        $matching = $this->match($requestMethod, $requestPath);
+        var_dump($matching);
+        if ($matching) {
+            $this->current = $matching;
+
+            return $matching->dispatch();
+        }
+
+        if (in_array($requestPath, $paths)) {
+            return $this->dispatchNotAllowed();
+        }
+
+        return $this->dispatchNotFound();
+    }
+
+    private function paths(): array
+    {
+        $paths = [];
+
+        foreach ($this->routes as $route) {
+            $paths[] = $route->path();
+        }
+
+        return $paths;
+    }
+
+    public function current(): ?Route
+    {
+        return $this->current;
+    }
+
+    private function match(string $method, string $path): ?Route
+    {
+        foreach ($this->routes as $route) {
+            if ($route->matches($method, $path)) {
+                return $route;
+            }
+        }
+
+        return null;
+    }
+
     public function dispatchNotAllowed()
     {
-        $this->errorHandlers[400] ??= fn() => "not allowed";
+        $this->errorHandlers[400] ??= fn() => 'not allowed';
         return $this->errorHandlers[400]();
     }
+
     public function dispatchNotFound()
     {
-        $this->errorHandlers[404] ??= fn() => "not found";
+        $this->errorHandlers[404] ??= fn() => 'not found';
         return $this->errorHandlers[404]();
     }
 
     public function dispatchError()
     {
-        $this->errorHandlers[500] ??= fn() => "server router dispatch error";
+        $this->errorHandlers[500] ??= fn() => 'server error';
         return $this->errorHandlers[500]();
     }
 
+    public function redirect($path) : void
+    {
+        header("Location: {$path}", $replace = true, $code = 301);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function route(string $name, array $parameters = []): string
+    {
+        foreach ($this->routes as $route) {
+            if ($route->name() === $name) {
+                $finds = [];
+                $replaces = [];
+
+                foreach ($parameters as $key => $value) {
+                    $finds[] = "{{$key}}";
+                    $replaces[] = $value;
+                    $finds[] = "{{$key}?}";
+                    $replaces[] = $value;
+                }
+
+                $path = $route->path();
+                $path = str_replace($finds, $replaces, $path);
+                return preg_replace('#{[^}]+}#', '', $path);
+            }
+        }
+
+        throw new Exception('no route with that name');
+    }
 }
