a9a7b65196a89f362af96aa85659d64f584f795d
halweg <haowei@joshine.com>
2024-02-25 11:56:50 +0800

    include helpers.php use composer

--- public/index.php
+++ public/index.php
@@ -2,9 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 $router = new Framework\Routing\Router();
-
 $routes = require_once __DIR__ . '/../app/routes.php';
-require_once __DIR__ . '/../framework/helpers.php';
 $routes($router);
 print $router->dispatch();
 
